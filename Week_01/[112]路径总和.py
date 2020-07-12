4459127bdd74eb9ed9ac78a3d09cc4e4a86c9a4e
sibodiamond <life121612@sina.com>
2020-07-13 00:20:19 +0800

    leetcode-112

---
+++ Week_01/[112]路径总和.py
@@ -0,0 +1,46 @@
+# 给定一个二叉树和一个目标和，判断该树中是否存在根节点到叶子节点的路径，这条路径上所有节点值相加等于目标和。 
+# 
+#  说明: 叶子节点是指没有子节点的节点。 
+# 
+#  示例: 
+# 给定如下二叉树，以及目标和 sum = 22， 
+# 
+#                5
+#              / \
+#             4   8
+#            /   / \
+#           11  13  4
+#          /  \      \
+#         7    2      1
+#  
+# 
+#  返回 true, 因为存在目标和为 22 的根节点到叶子节点的路径 5->4->11->2。 
+#  Related Topics 树 深度优先搜索
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    
+    def hasPathSum(self, root: TreeNode, sum: int) -> bool:
+        if not root:
+            return False
+        sum -= root.val
+        if not root.left and not root.right:
+            return sum == 0
+        else:
+            return self.hasPathSum(root.left, sum) or self.hasPathSum(root.right, sum)
+
+
+
+
+
+
+
+# leetcode submit region end(Prohibit modification and deletion)
