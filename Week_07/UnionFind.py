9a513b617c9554e7a1223f2613481069258febf4
sibodiamond <life121612@sina.com>
2020-08-24 10:24:03 +0800

    UnionFind

---
+++ Week_07/UnionFind.py
@@ -0,0 +1,18 @@
+class UnionFind:
+    def __init__(self, n):
+        self.p = [i for i in range(n)]
+
+    def union(self, i, j):
+        p1 = self.parent(i)
+        p2 = self.parent(j)
+        self.p[p1] = p2
+
+    def parent(self, i):
+        root = i
+        while self.p[root] != root:
+            root = self.p[root]
+        # root 指向i的parent
+        # 下面loop,使得i到root之间的节点的parent均指向root.
+        while self.p[i] != i:
+            x = i; i = self.p[i]; self.p[x] = root
+        return root
